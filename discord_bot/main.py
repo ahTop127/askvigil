4e72422087b1b2ec00d36b896ed3b1c755be6e40
JiaYiBee <jiayibee777@gmail.com>
2026-05-16 20:21:16 +0800

    debugging discord bot

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -13,7 +13,7 @@ import asyncio
 
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-ASKVIGIL_API_URL = os.getenv("ASKVIGIL_API_URL")
+ASKVIGIL_API_URL = https://askvigil.duckdns.org/api/v1/detection/scan
 
 
 class AftercareView(discord.ui.View):
